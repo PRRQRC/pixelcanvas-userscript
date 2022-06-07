3f31d8eafd784e082cc4876e95828cc259c9e6fe
ShadowLp174 <samuel.moravec@yahoo.com>
2022-06-07 22:29:17 +0200

    update: commands with token reset

--- userscript.user.js
+++ userscript.user.js
@@ -2,7 +2,6 @@
 // @name        PlaceRickQRCode PixelCanvas
 // @namespace   PlaceRickQRCode
 // @match       *://pixelcanvas.io/*
-// @match       http://localhost:3000/
 // @grant       none
 // @require     https://cdn-shadowlp174.4lima.de/modal.js
 // @version     1.0
@@ -20,14 +19,14 @@ gateway.style.height = "27%";
 //document.body.append(gateway);
 
 // console class;
-class Log { constructor() { return this } format(e) { function t(e) { return e < 10 ? "0" + e : e } return t(e.getHours()) + ":" + t(e.getMinutes()) + ":" + t(e.getSeconds()) } log(e) { e = "string" != typeof e ? JSON.stringify(e) : e; var t = document.querySelector(".logs"); 0 == messageCount && (t.innerHTML = ""); var n = document.createElement("span"); n.classList.add("time"), n.innerHTML = "[" + this.format(new Date) + "]"; var r = document.createElement("li"); r.innerHTML = e, r.prepend(n), t.appendChild(r), messageCount++, r.scrollIntoView() } error(...e) { for (let s = 0; s < e.length; s++) { let o = e[s]; o = "string" != typeof o ? JSON.stringify(o) : o; var t = document.querySelector(".logs"); 0 == messageCount && (t.innerHTML = ""); var n = document.createElement("span"); n.classList.add("time"), n.innerHTML = "[" + this.format(new Date) + "]"; var r = document.createElement("li"); r.style.color = "#F62451", r.innerHTML = o, r.prepend(n), t.appendChild(r), messageCount++, r.scrollIntoView() } } success(e) { e = "string" != typeof e ? JSON.stringify(e) : e; var t = document.querySelector(".logs"); 0 == messageCount && (t.innerHTML = ""); var n = document.createElement("span"); n.classList.add("time"), n.innerHTML = "[" + this.format(new Date) + "]"; var r = document.createElement("li"); r.style.color = "#1fd78d", r.innerHTML = e, r.prepend(n), t.appendChild(r), messageCount++, r.scrollIntoView() } warn(e) { e = "string" != typeof e ? JSON.stringify(e) : e; var t = document.querySelector(".logs"); 0 == messageCount && (t.innerHTML = ""); var n = document.createElement("span"); n.classList.add("time"), n.innerHTML = "[" + this.format(new Date) + "]"; var r = document.createElement("li"); r.style.color = "#EB7B59", r.innerHTML = e, r.prepend(n), t.appendChild(r), messageCount++, r.scrollIntoView() } }
+class CommandHandler { constructor(a, b) { return this.commands = [], this.callbacks = [], this.prefix = a, this.error = b, this.history = [], this } addCommand(a, b) { this.commands.push(a), this.callbacks.push(b) } executeCommand(a, e) { let b = a.repeat(1); this.history.unshift(b); let c = this.prefix, f = a.replace(c, "").toLowerCase().split(" ").slice(1); a = a.replace(c, "").toLowerCase().split(" ")[0]; let d = this.commands.indexOf(a); if (-1 === d) { this.error(b, e); return } this.callbacks[d](f) } getHistory(a) { return a < 0 || a >= this.history.length ? null : this.history[a] } } class Log { constructor(a) { return this.container = document.querySelector(".console"), this.input = document.querySelector(".input"), this.input && (this.currentPos = 0, this.input.addEventListener("keyup", a => { 13 === a.keyCode && (this.currentPos = 0, this.processCommand(this.input.value), this.input.value = "") }), this.input.addEventListener("keyup", b => { if (38 === b.keyCode) { if (!this.commandHandler) return; let a = this.commandHandler; !(this.currentPos >= a.history.length) && (this.input.value = a.getHistory(this.currentPos), this.currentPos++) } }), this.input.addEventListener("keyup", a => { if (40 === a.keyCode) { if (!this.commandHandler) return; let b = this.commandHandler; this.currentPos <= 0 || (this.input.value = b.getHistory(--this.currentPos)) } })), this.commandHandler = a, this } processCommand(a) { if (!this.commandHandler || !a.startsWith(this.commandHandler.prefix)) { this.log(a); return } this.commandHandler.executeCommand(a, this) } format(a) { function b(a) { return a < 10 ? "0" + a : a } return b(a.getHours()) + ":" + b(a.getMinutes()) + ":" + b(a.getSeconds()) } log(a) { a = "string" != typeof a ? JSON.stringify(a) : a; var d = document.querySelector(".logs"); 0 == messageCount && (d.innerHTML = ""); var c = document.createElement("span"); c.classList.add("time"), c.innerHTML = "[" + this.format(new Date) + "]"; var b = document.createElement("li"); b.innerHTML = a, b.prepend(c), d.appendChild(b), messageCount++, b.scrollIntoView() } error(...e) { for (let c = 0; c < e.length; c++) { let a = e[c]; a = "string" != typeof a ? JSON.stringify(a) : a; var f = document.querySelector(".logs"); 0 == messageCount && (f.innerHTML = ""); var d = document.createElement("span"); d.classList.add("time"), d.innerHTML = "[" + this.format(new Date) + "]"; var b = document.createElement("li"); b.style.color = "#F62451", b.innerHTML = a, b.prepend(d), f.appendChild(b), messageCount++, b.scrollIntoView() } } success(a) { a = "string" != typeof a ? JSON.stringify(a) : a; var d = document.querySelector(".logs"); 0 == messageCount && (d.innerHTML = ""); var c = document.createElement("span"); c.classList.add("time"), c.innerHTML = "[" + this.format(new Date) + "]"; var b = document.createElement("li"); b.style.color = "#1fd78d", b.innerHTML = a, b.prepend(c), d.appendChild(b), messageCount++, b.scrollIntoView() } warn(a) { a = "string" != typeof a ? JSON.stringify(a) : a; var d = document.querySelector(".logs"); 0 == messageCount && (d.innerHTML = ""); var c = document.createElement("span"); c.classList.add("time"), c.innerHTML = "[" + this.format(new Date) + "]"; var b = document.createElement("li"); b.style.color = "#EB7B59", b.innerHTML = a, b.prepend(c), d.appendChild(b), messageCount++, b.scrollIntoView() } }
 
-const style = '* {--color-dark: #2D3943;--color-light: #f0f0f0;} .console {text-align:left;background-color: var(--color-dark);padding: 5px;border-radius: 5px;max-height: 80vw;overflow-y: auto;font-family: monospace;}.logs {padding-right: 0;padding-left: 2%;}.logs li {margin: 7px 0 7px 0;color: var(--color-light);text-align:left}.logs li::marker {color: #777;}.time {color: #2d87d3;margin-right: 6px;}'
+const style = '*{--color-dark:#2D3943;--color-light:#f0f0f0}.console{background-color:var(--color-dark);padding:5px;border-radius:5px;max-height:80vw;overflow-y:auto;font-family:monospace;display:flex;flex-direction:column}.logs{padding-right:0;padding-left:2%}.logs li{margin:7px 0 7px 0;color:var(--color-light)}.logs li::marker{color:#777}.time{color:#2d87d3;margin-right:6px}.input{width:99%;position:relative;bottom:0;align-self:center;background:inherit;border:inherit;padding-top:5px;padding-bottom:5px;margin-top:0;color:var(--color-light)}';
 const styleElem = document.createElement("style");
 styleElem.innerHTML = style;
 document.head.appendChild(styleElem);
 
-var consoleElement = '<div class="console" style="position: absolute; height: 27%; width: 15%; top:5px; right:5px; z-index: 10000; -webkit-box-shadow: 13px 5px 15px 5px rgba(0,0,0,0.50);box-shadow: 13px 5px 15px 5px rgba(0,0,0,0.50);"><ol class="logs"></ol></div>';
+var consoleElement = `<div class="console" style="position: absolute; height: 27%; width: 18%; top:5px; right:5px; z-index: 10000; -webkit-box-shadow: 13px 5px 15px 5px rgba(0,0,0,0.50);box-shadow: 13px 5px 15px 5px rgba(0,0,0,0.50);"> <ol class="logs"></ol> <input class="input" placeholder="Type your command here..."/></div>`;
 document.body.appendChild(toHTML(consoleElement));
 
 const promptContent = `<center> <p>Please enter your bot token!<br/>You can find it <a href="https://tempauthserver.shadowlp174.repl.co" target="_blank" style="color: #328dd2; text-decoration: underline;">here</a> by logging in with your Discord account. </p><br/> <div> <input type="text" placeholder="Your Token"/> <button id="checkToken">Check</button> </div></center>`;
@@ -47,8 +46,27 @@ var time;
 var fingerprint;
 var token = getCookie("bottoken");
 var socket;
+var isInTimeout = false;
 
-const logs = new Log();
+const handler = new CommandHandler("/", (cmd, log) => {
+  log.log("Unknown command: '" + cmd + "'");
+});
+handler.addCommand("token", (args) => {
+  if (args[0]) {
+    logs.warn("Verifying token '" + args[0] + "'");
+    verify(args[0], () => {
+      logs.warn("Token not valid!");
+    }, () => {
+      logs.success("Token valid. Now using token '" + args[0] + "'");
+    });
+  } else {
+    eraseCookie("bottoken");
+    getBotToken();
+    logs.warn("Reset Token. Please enter a new one.");
+  }
+});
+
+const logs = new Log(handler);
 logs.log("Initializing...");
 
 
@@ -113,14 +131,32 @@ function processMessage(message) {
   }
 
   switch (data.type) {
+    case "intentional-error":
     case "error":
       logs.warn(message);
+      processError(data);
+      break;
+    case "revive":
+      if (isInTimeout) return;
       break;
     case "pixel":
       processPixel(data.pixel);
       break;
     default:
       logs.log(message);
+      break;
+  }
+}
+
+function processError(data) {
+  switch (data.errorCode) {
+    case 10:
+      logs.log("Retrying in 1 min...");
+      setTimeout(() => {
+        drawNext();
+      }, 60000);
+      break;
+    default:
       break;
   }
 }
@@ -149,7 +185,9 @@ function drawNext() {
   } else {
     let timeout = (time + ((3 * 60 + 10) * 1000) - (new Date()).getTime());
     logs.log("Scheduling next pixel... Ordering in " + (Math.round((timeout / 1000 * 10)) / 10) + " seconds (" + (Math.round((timeout / 1000 / 60 * 10)) / 10) + " min).");
+    isInTimeOut = true;
     setTimeout(() => {
+      isInTimeOut = false;
       drawNext();
     }, timeout);
   }
@@ -188,9 +226,9 @@ function paintPixel(pixel) {
 function getBotToken() {
   logs.log("Searching for bot token in cookies...");
   let t = getCookie("bottoken");
-  if (!t) {
-    setCookie("bottoken", "test", 2);
-    if (!getCookie("bottoken")) {
+  if (!t || t == "") {
+    setCookie("init", "checking", 1);
+    if (!getCookie("init")) {
       logs.error("Please enable cookies in your browser in order to use this userscript!");
       return;
     }
@@ -198,10 +236,11 @@ function getBotToken() {
     modal.show();
     return;
   }
-  logs.log("Token found!");
+  logs.log("Token found! Using: " + t);
   init();
 }
 
+var verify;
 function setupTokenCheck() {
   const btn = document.getElementById("checkToken");
   const input = btn.parentElement.children[0];
@@ -211,23 +250,28 @@ function setupTokenCheck() {
     btn.innerText = "Checking token...";
     btn.disabled = true;
 
-    fetch("https://tempauthserver.shadowlp174.repl.co/api/verify/" + t).then(res => {
-      res.json().then(json => {
-        if (json.type == "success") {
-          modal.hide();
-          setCookie("bottoken", t);
-          token = t;
-          init();
-          return;
-        }
-        btn.disabled = false;
-        input.disabled = false;
-        btn.innerText = "Token Invalid! Check Again";
-        window.alert("Your token is invalid or there was an error.");
-      }).catch((err) => {
-        console.log(err);
+    verify = (key, e, s) => {
+      fetch("https://tempauthserver.shadowlp174.repl.co/api/verify/" + key).then(res => {
+        res.json().then(json => {
+          if (json.type == "success") {
+            modal.hide();
+            setCookie("bottoken", t, 10);
+            token = key;
+            init();
+            if (s) s();
+            return;
+          }
+          btn.disabled = false;
+          input.disabled = false;
+          btn.innerText = "Token Invalid! Check Again";
+          window.alert("Your token is invalid or there was an error.");
+          if (e) e();
+        }).catch((err) => {
+          console.log(err);
+        });
       });
-    });
+    }
+    verify(t);
   }
 }
 
@@ -251,6 +295,9 @@ function setCookie(name, value, days) {
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function eraseCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
 }
 window.post = (url, data) => {
   return fetch(url, {
@@ -279,3 +326,4 @@ function formatDate(epoch) {
     localeString: date.toLocaleString()
   }
 }
+
